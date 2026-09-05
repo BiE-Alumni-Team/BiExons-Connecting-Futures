a8721dd2c4877a77d783c5e505c2f1b09bd8ccf9
7ahseeen <202901182+7ahseeen@users.noreply.github.com>
2026-09-05 16:59:20 +0600

    Added DB info ---- SEPERETE SENSITIVE INFO LATER ----    . Added id field in User class. Done serializer.py

--- auth_back/login/accounts/models.py
+++ auth_back/login/accounts/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    reg_no = models.CharField(max_length=20, unique=True)
+    id_no = models.CharField(max_length=20, unique=True)
+    session = models.CharField(max_length=20)
+    batch = models.CharField(max_length=20, blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', blank=True, null=True)
+    workplace = models.CharField(max_length=200, blank=True, null=True)
+    designation = models.CharField(max_length=100, blank=True, null=True)
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.username} ({self.reg_no})"
