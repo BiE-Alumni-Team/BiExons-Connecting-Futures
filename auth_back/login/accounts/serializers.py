1231dcd884776b4dd5cd9f5343a6362dc23bd1fa
7ahseeen <202901182+7ahseeen@users.noreply.github.com>
2026-09-08 13:06:04 +0600

    removed username

--- auth_back/login/accounts/serializers.py
+++ auth_back/login/accounts/serializers.py
@@ -10,7 +10,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
-            'id', 'username', 'email', 'reg_no', 'id_no', 'first_name', 'last_name',
+            'id', 'email', 'reg_no', 'id_no', 'first_name', 'last_name',
             'session', 'phone', 'password',
         ]
 
